c621242c6884810f0c43625a2ee2d0ebc91905c7
MrBerts <berthier.gllm@gmail.com>
2017-01-21 18:21:30 +0100

    find next cells to visit (wip)

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var fs = require('fs');
 
 var wallsFile;
 var maze = [];
+var solution = [];
 
 // App route
 var app = express();
@@ -37,7 +38,9 @@ app.post('/upload-file', upload.single('maze-file'), function(req,res){
     //Vérification du fichier
 
     wallsFile = obj;
+
     buildMaze();
+    solveMaze();
 
     //io.sockets.emit('message', 'bonjour');
     // Go back to index.html
@@ -46,7 +49,6 @@ app.post('/upload-file', upload.single('maze-file'), function(req,res){
 });
 
 function buildMaze() {
-    console.log(wallsFile);
 
     // Init the maze array
     var i, j;
@@ -57,10 +59,9 @@ function buildMaze() {
         }
     }
 
-    // Define walls
+    // Define walls and path
     var cellX1, cellY1, cellX2, cellY2, wallX, wallY, temp;
     wallsFile.walls.forEach(function(tuple) {
-        console.log(tuple);
         cellX1 = (tuple[0][0] + 1) * 2 - 1;
         cellY1 = (tuple[0][1] + 1) * 2 - 1;
         cellX2 = (tuple[1][0] + 1) * 2 - 1;
@@ -69,32 +70,107 @@ function buildMaze() {
         wallY = (cellY1 + cellY2) / 2;
 
         maze[wallX][wallY] = 0;
-
-        if (wallX == cellX1) {
-
-            temp = wallX - 1;
-            if (temp != 0) {
-                maze[temp][wallY] = 0;
-            }
-
-            temp = wallX + 1;
-            if (temp != wallsFile.height * 2) {
-                maze[temp][wallY] = 0;
-            }
-        } else if (wallY == cellY1) {
-            
-            temp = wallY - 1;
-            if (temp != 0) {
-                maze[wallX][temp] = 0;
-            }
-
-            temp = wallY + 1
-            if (temp != wallsFile.width * 2) {
-                maze[wallX][temp] = 0;   
-            }
-        }
-        console.log(maze);
     });
 
+    //Define start
+    maze[wallsFile.start[0] * 2 + 1][wallsFile.start[1] * 2 + 1] = 2;
+
+    //Define end
+    maze[wallsFile.end[0] * 2 + 1][wallsFile.end[1] * 2 + 1] = 3;    
     
+    //console.log(maze);
+}
+
+function solveMaze() {
+
+    //                left     down    right   up
+    var direction = [[0, -1], [1, 0], [0, 1], [-1, 0]];
+    var move =      [[0, -2], [2, 0], [0, 2], [-2, 0]];
+    var current = [wallsFile.start[0] + 1, wallsFile.start[1] + 1];
+    var previous = current
+    var next = [];
+    
+    solution.push(current);
+
+    var deltaX = 2, deltaY = 0, temp;
+    var signe = 1;
+
+    // Test win
+    if (current == wallsFile.end) {
+        return result;
+    }
+
+    // Find the first next from start
+    var i = -1;
+    do {
+        i++;
+        next[0] = current[0] + direction[i][0];
+        next[1] = current[1] + direction[i][1];
+    } while (!testCell(next))
+
+    next[0] = current[0] + move[i][0];
+    next[1] = current[1] + move[i][1];
+    previous = current;
+    current = next;
+    console.log("previous", previous);
+    console.log("current", current);
+    testPush(current);
+
+    // Find a path
+    while (maze[current[0]][current[1]] != 3) {
+
+        temp = [previous[0] - current[0], previous[1] - current[1]];
+        console.log("temp", temp);
+        // Find next cell
+        if (temp[0] < 0) {
+            i = 0;
+        } else if (temp[0] > 0) {
+            i = 2;
+        } else if (temp[1] < 0) {
+            i = 1;
+        } else if (temp[1] > 0) {
+            i = 3;
+        } else {
+            console.log("Error");
+            return -1;
+        }
+
+        do {
+            next[0] = current[0] + direction[i][0];
+            next[0] = current[0] + direction[i][0];
+            i++;
+            i %= direction.length;
+        } while (!testCell(next))
+
+        i--;
+        
+        next[0] = current[0] + move[i][0];
+        next[1] = current[1] + move[i][1];
+
+        previous = current;
+        current = next;
+        console.log("i", i, "next", next, "previous", previous, "current", current);
+        testPush(current);
+        console.log(maze);
+    }
+}
+
+function testCell(cell) {
+
+    if (cell[0] <= 0 || cell[0] >= wallsFile.height * 2 || cell[1] <= 0 || cell[1] >= wallsFile.width * 2) {
+        return false;
+    } else if (maze[cell[0]][cell[1]] == 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function testPush(cell) {
+
+    if (solution[solution.length - 1] == cell) {
+        solution.pop();
+    } else {
+        solution.push(cell);
+    }
 }
